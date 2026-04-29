4872688bdfc00a43e39bc0067bd72abbfd51b805
githubusername <emailusedingithub>
2026-04-29 14:50:35 +0300

    condition and loop

---
+++ conditionexercise.py
@@ -0,0 +1,9 @@
+grade = 'B'
+
+match grade:
+    case 'A':
+        print("Excellent!")
+    case 'B':
+        print("Good!")
+    case _:
+        print("Not specified.")
---
+++ loop-for.py
@@ -0,0 +1,15 @@
+
+fruits=[ "mango", "orange", "banana"]
+
+for i in fruits:
+    print(i)
+    
+#10list
+    lists=[ 1234567890]
+    for i in lists:
+        print("richard")
+ #create a list of number 1-100
+
+        lst= list(range(1,100))      
+        for i in lst:
+            print(lst)
